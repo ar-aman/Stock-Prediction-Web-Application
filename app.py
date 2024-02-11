29049932d9e44efdec9281c1ecde0b349b8271b1
ar-aman <111566230+ar-aman@users.noreply.github.com>
2024-02-11 20:03:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
-import pandas_datareader as data
-#import yfinance as yf #library for yahoo finance stock data
+#import pandas_datareader as data
+import yfinance as yf #library for yahoo finance stock data
 from datetime import datetime
