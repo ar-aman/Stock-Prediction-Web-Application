966c4b7011e5b8e595502a35374605818ed6e957
ar-aman <111566230+ar-aman@users.noreply.github.com>
2024-02-11 20:04:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
 #import pandas_datareader as data
-import yfinance as yf #library for yahoo finance stock data
+#import yfinance as yf #library for yahoo finance stock data
 from datetime import datetime
+start = '2010-01-01'
+end = datetime.today().strftime('%Y-%m-%d')
+
+df= yf.download('AAPL', start, end)
+df.head()
